13d7348815c49f9631ef88c998d2cc089ce7b9f6
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-25 20:07:01 +0600

    u

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,7 +5,7 @@ export default function ProductCard({productName, productDescription, productPri
     <div className="flex flex-col bg-white p-4 rounded-lg shadow-lg w-[100%] h-full">
       <div className="text-xl font-semibold">{productName}</div>
       <div className="text-lg">{productDescription}</div>
-      <div className="text-xl font-semibold">${productPrice}</div>
+      <div className="text-xl font-semibold">$ {productPrice}</div>
     </div>
   )
     
